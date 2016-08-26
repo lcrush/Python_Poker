d36323a4302add194f8c4fa41ff409d1ef5e30a7
Lisa Rush <lisa.rush@my.denver.coloradotech.edu>
2016-08-25 19:50:40 -0600

    Updated

--- test_hand_eval.py
+++ test_hand_eval.py
@@ -6,7 +6,7 @@ Created on Tue Aug 16 17:39:00 2016
 """
 
 from pokerdeck import *
-from Poker_mid import *
+from Poker_Midterm_test import *
 
 def test_deal_hand():
     assert(len(deal_hand()) == 5)
@@ -16,3 +16,15 @@ def test_no_dupes():
     for card in hand:
         assert hand.count(card) == 1
 
+# pair_hand = [Card('9', '♦'), Card('9', '♥'), Card('4', '♠'), Card('J', '♠'), Card('Q', '♠')]
+# no_pair_hand = [Card('8', '♦'), Card('9', '♥'), Card('4', '♠'), Card('J', '♠'), Card('Q', '♠')]
+
+
+def test_find_pairs():
+    pairs = [Card('8', '♦'), Card('K', '♥'), Card('K', '♠'), Card('J', '♠'), Card('4', '♠')]
+    assert find_ranks(pairs) == ['8', 'K', 'K', '4', 'J']
+
+
+def test_find_set():
+    three_kind = [Card('K', '♦'), Card('K', '♥'), Card('K', '♠'), Card('J', '♠'), Card('4', '♠')]
+    assert find_set(ranks) == ['K', 'K', 'K', 'J', '4']
